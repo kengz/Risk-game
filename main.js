a5c761e9a0e7c067388320bafcd5e025dde3c4ea
kengz <kengzwl@gmail.com>
2015-04-10 16:33:12 -0400

    added dataset 1_2 and 1_10

--- main.js
+++ main.js
@@ -34,12 +34,12 @@ var start = new Date().getTime();
 // array: pers1, pers2, firstplayer
 // note: 1,2 are indices for p1, p2 resp
 // repeat(3, [1,1,1], './data/GS_test_1_1_1.json');
-repeat(100, [1,1,1], './data/GS_1_1_1.json');
-repeat(100, [1,1,2], './data/GS_1_1_2.json');
-repeat(100, [1,2,1], './data/GS_1_2_1.json');
+// repeat(100, [1,1,1], './data/GS_1_1_1.json');
+// repeat(100, [1,1,2], './data/GS_1_1_2.json');
+// repeat(100, [1,2,1], './data/GS_1_2_1.json');
 // repeat(100, [1,2,2], './data/GS_1_2_2.json');
-// repeat(100, [1,10,1], './data/GS_1_10_1.json');
-// repeat(100, [1,10,2], './data/GS_1_10_2.json');
+repeat(100, [1,10,1], './data/GS_1_10_1.json');
+repeat(100, [1,10,2], './data/GS_1_10_2.json');
 // repeat(100, [1,13,1], './data/GS_1_13_1.json');
 // repeat(100, [1,13,2], './data/GS_1_13_2.json');
 // repeat(100, [2,10,1], './data/GS_2_10_1.json');
