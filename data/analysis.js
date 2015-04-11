28cab10d97d8183139023b29c526ea2dd66990eb
kengz <kengzwl@gmail.com>
2015-04-10 20:55:52 -0400

    Added longer run(500 games)

--- data/analysis.js
+++ data/analysis.js
@@ -3,10 +3,14 @@ var _ = require('underscore');
 var fs = require('fs');
 
 // var GS = require('./GS_test_1_1_1.json');
-var GS = require('./GS_1_1_1.json');
+// var GS = require('./GS_1_1_1.json');
 // var GS = require('./GS_1_1_2.json');
+// var GS = require('./GS_1_1_1_long.json');
+// var GS = require('./GS_1_1_2_long.json');
 // var GS = require('./GS_1_2_1.json');
 // var GS = require('./GS_1_2_2.json');
+// var GS = require('./GS_1_2_1_500.json');
+// var GS = require('./GS_1_2_2_500.json');
 // var GS = require('./GS_2_10_1.json');
 // var GS = require('./GS_2_10_2.json');
 // var GS = require('./GS_10_13_1.json');
@@ -48,7 +52,7 @@ function pull(j) {
 	// set the game_number
 	game_n = j;
 	// the number of turns in a game = game.length - 4
-	var n_turn = _.keys(GS[game_n]).length - 4;
+	// var n_turn = _.keys(GS[game_n]).length - 4;
 	// return _.map(_.range(1, n_turn+1), result);
 	return result(j);
 }
@@ -63,6 +67,7 @@ function pull(j) {
 function pullAll() {
 	// total number of games played
 	var num_game = _.keys(GS).length;
+	// return _.map(_.range(1, num_game+1), pull)
 	return _.map(_.range(1, num_game+1), pull)
 }
 
@@ -84,5 +89,5 @@ function counter(p) {
 console.log(counter('p1').win);
 console.log(counter('p2').win);
 
-console.log(counter('p1').lose);
-console.log(counter('p2').lose);
+// console.log(counter('p1').lose);
+// console.log(counter('p2').lose);
